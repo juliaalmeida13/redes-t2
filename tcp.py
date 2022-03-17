71ede23687f4ad7daf9253409f81135ae95e2295
Júlia Almeida <julia.almeida@estudante.ufscar.br>
2022-03-17 18:07:07 -0300

    passos 5 e 6

--- tcp.py
+++ tcp.py
@@ -1,9 +1,91 @@
 import asyncio
+from curses import flash
 from random import randint
-from time import time
+from sys import flags
 from tcputils import *
+import struct
+from time import time
+
+# Valores das flags que serão usadas na nossa implementação simplificada
+FLAGS_FIN = 1<<0
+FLAGS_SYN = 1<<1
+FLAGS_RST = 1<<2
+FLAGS_ACK = 1<<4
+
+MSS = 1460   # Tamanho do payload de um segmento TCP (em bytes)
+
+def make_header(src_port, dst_port, seq_no, ack_no, flags):
+    """
+    Constrói um cabeçalho TCP simplificado.
+    Consulte o formato completo em https://en.wikipedia.org/wiki/Transmission_Control_Protocol#TCP_segment_structure
+    """
+    return struct.pack('!HHIIHHHH',
+                       src_port, dst_port, seq_no, ack_no, (5 << 12) | flags,
+                       8*MSS, 0, 0)
 
 
+def read_header(segment):
+    """
+    Lê um cabeçalho
+    """
+    src_port, dst_port, seq_no, ack_no, \
+        flags, window_size, checksum, urg_ptr = \
+        struct.unpack('!HHIIHHHH', segment[:20])
+    return src_port, dst_port, seq_no, ack_no, \
+        flags, window_size, checksum, urg_ptr
+
+
+def calc_checksum(segment, src_addr=None, dst_addr=None):
+    """
+    Calcula o checksum complemento-de-um (formato do TCP e do UDP) para os
+    dados fornecidos.
+    É necessário passar os endereços IPv4 de origem e de destino, já que
+    apesar de não fazerem parte da camada de transporte, eles são incluídos
+    no pseudocabeçalho, que faz parte do cálculo do checksum.
+    Os endereços IPv4 devem ser passados como string (no formato x.y.z.w)
+    """
+    if src_addr is None and dst_addr is None:
+        data = segment
+    else:
+        pseudohdr = str2addr(src_addr) + str2addr(dst_addr) + \
+            struct.pack('!HH', 0x0006, len(segment))
+        data = pseudohdr + segment
+
+    if len(data) % 2 == 1:
+        # se for ímpar, faz padding à direita
+        data += b'\x00'
+    checksum = 0
+    for i in range(0, len(data), 2):
+        x, = struct.unpack('!H', data[i:i+2])
+        checksum += x
+        while checksum > 0xffff:
+            checksum = (checksum & 0xffff) + 1
+    checksum = ~checksum
+    return checksum & 0xffff
+
+
+def fix_checksum(segment, src_addr, dst_addr):
+    """
+    Corrige o checksum de um segmento TCP.
+    """
+    seg = bytearray(segment)
+    seg[16:18] = b'\x00\x00'
+    seg[16:18] = struct.pack('!H', calc_checksum(seg, src_addr, dst_addr))
+    return bytes(seg)
+
+
+def addr2str(addr):
+    """
+    Converte um endereço IPv4 binário para uma string (no formato x.y.z.w)
+    """
+    return '%d.%d.%d.%d' % tuple(int(x) for x in addr)
+
+
+def str2addr(addr):
+    """
+    Converte uma string (no formato x.y.z.w) para um endereço IPv4 binário
+    """
+    return bytes(int(x) for x in addr.split('.'))
 class Servidor:
     def __init__(self, rede, porta):
         self.rede = rede
@@ -34,31 +116,27 @@ class Servidor:
         id_conexao = (src_addr, src_port, dst_addr, dst_port)
 
 
-        # Passo 1: aceitar a conexão com flag SYN+ACK
+        # Passo 1
 
         if (flags & FLAGS_SYN) == FLAGS_SYN:
-
             # A flag SYN estar setada significa que é um cliente tentando estabelecer uma conexão nova
+            # TODO: talvez você precise passar mais coisas para o construtor de conexão            
             conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao)
-            
+            # TODO: você precisa fazer o handshake aceitando a conexão. Escolha se você acha melhor
+            # fazer aqui mesmo ou dentro da classe Conexao.
             # Criando as flags
             flags = flags & 0
             flags = flags | (FLAGS_SYN | FLAGS_ACK)
 
-            # Alterando valor de ACK_NO como um número aleatório
             conexao.seq_no = randint(0, 0xffff)
             conexao.ack_no = seq_no + 1
-
-            # Invertendo o endereço de origem e de destino
             src_port, dst_port = dst_port, src_port
             src_addr, dst_addr = dst_addr, src_addr
 
-            # Construindo o cabeçalho com flags SYN e ACK
             segmento = make_header(src_port, dst_port, conexao.seq_no, conexao.ack_no, flags)
             segmento_checksum_corrigido = fix_checksum(segmento, src_addr, dst_addr)
             self.rede.enviar(segmento_checksum_corrigido, dst_addr)
 
-            # Incrementando seq_no para considerar o SYN enviado
             conexao.seq_no += 1
             conexao.seq_no_base = conexao.seq_no
 
@@ -76,31 +154,37 @@ class Conexao:
     def __init__(self, servidor, id_conexao):
         self.servidor = servidor
         self.id_conexao = id_conexao
+        self.callback = None
+        #self.timer = asyncio.get_event_loop().call_later(1, self._exemplo_timer)  # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
+        self.timer = None
+        self.timeoutInterval = 1
+        #Passo1
         self.seq_no = None
         self.ack_no = None
-        self.callback = None
-        self.timer = None
         self.seq_no_base = None
+
         self.pacotes_sem_ack = []
-        self.timeoutInterval = 1
+        #self.timer.cancel()   # é possível cancelar o timer chamando esse método; esta linha é só um exemplo e pode ser removida
         self.devRTT = None
         self.estimatedRTT = None
         self.fila_envio = []
 
+    #def _exemplo_timer(self):
+        # Esta função é só um exemplo e pode ser removida
+        #print('Este é um exemplo de como fazer um timer')
 
-    # Passo 5: montando o timer (?)
+    # Passo 5: Timer
 
     def _timer(self):
         if self.pacotes_sem_ack:
             segmento, _, dst_addr, _ = self.pacotes_sem_ack[0]
-            # Reenviando pacote
+
             self.servidor.rede.enviar(segmento, dst_addr)
             self.pacotes_sem_ack[0][3] = None
 
 
     # Passo 6: calculando o TimeoutInterval
-
-    def _atualizar_timeout_interval(self):
+    def timeout_interval(self):
         _, _, _, sampleRTT = self.pacotes_sem_ack[0]
         if sampleRTT is None:
             return
@@ -117,42 +201,37 @@ class Conexao:
 
 
     def _rdt_rcv(self, seq_no, ack_no, flags, payload):
+        # TODO: trate aqui o recebimento de segmentos provenientes da camada de rede.
+        # Chame self.callback(self, dados) para passar dados para a camada de aplicação após
+        # garantir que eles não sejam duplicados e que tenham sido recebidos em ordem.
         print('recebido payload: %r' % payload)
 
-
-	# Passo 2: verificando os segmentos e pacotes da camada de rede enviados
-
-        # Se der flag ACK, precisa encerrar o timer e remover da lista de pacotes que precisam ser confirmados
         if (flags & FLAGS_ACK) == FLAGS_ACK and ack_no > self.seq_no_base:
             self.seq_no_base = ack_no
             if self.pacotes_sem_ack:
-                self._atualizar_timeout_interval()
+                self.timeout_interval()
                 self.timer.cancel()
                 self.pacotes_sem_ack.pop(0)
                 if self.pacotes_sem_ack:
                     self.timer = asyncio.get_event_loop().call_later(self.timeoutInterval, self._timer)
 
-        # Se for um pedido de encerrar a conexão
+        #Lidando com o Passo 2 aqui
         if (flags & FLAGS_FIN) == FLAGS_FIN:
             payload = b''
             self.ack_no += 1
         elif len(payload) <= 0:
             return
 
-	# Verificando se o pacote não é duplicado ou se está fora de ordem
         if seq_no != self.ack_no:
             return
-
         self.callback(self, payload)
         self.ack_no += len(payload)
 
-        # Construindo e enviando pacote ACK
         dst_addr, dst_port, src_addr, src_port = self.id_conexao
+
         segmento = make_header(src_port, dst_port, self.seq_no_base, self.ack_no, FLAGS_ACK)
         segmento_checksum_corrigido = fix_checksum(segmento, src_addr, dst_addr)
-
         self.servidor.rede.enviar(segmento_checksum_corrigido, dst_addr)
-
     # Os métodos abaixo fazem parte da API
 
     def registrar_recebedor(self, callback):
@@ -166,11 +245,11 @@ class Conexao:
         """
         Usado pela camada de aplicação para enviar dados
         """
+        # TODO: implemente aqui o envio de dados.
+        # Chame self.servidor.rede.enviar(segmento, dest_addr) para enviar o segmento
+        # que você construir para a camada de rede.
 
-
-	# Passo 2 e 3: envio dos dados recebidos dos pacotes e dados
-
-        # Construindo e enviando os pacotes
+        #Passo 3 
         dst_addr, dst_port, src_addr, src_port = self.id_conexao
 
         flags = 0 | FLAGS_ACK
@@ -183,8 +262,8 @@ class Conexao:
 
             segmento = make_header(src_port, dst_port, self.seq_no, self.ack_no, flags)
             segmento_checksum_corrigido = fix_checksum(segmento+payload, src_addr, dst_addr)
+            
             self.servidor.rede.enviar(segmento_checksum_corrigido, dst_addr)
-
             self.timer = asyncio.get_event_loop().call_later(self.timeoutInterval, self._timer)
             self.pacotes_sem_ack.append( [segmento_checksum_corrigido, len(payload), dst_addr, round(time(), 5)] )
 
@@ -196,4 +275,11 @@ class Conexao:
         Usado pela camada de aplicação para fechar a conexão
         """
         # TODO: implemente aqui o fechamento de conexão
+        #Passo 4
+        dst_addr, dst_port , src_addr , src_port = self.id_conexao
+
+        seg = make_header (src_port,dst_port,self.seq_no + 1,self.ack_no, FLAGS_FIN | FLAGS_ACK)
+        seg_checksum_ver = fix_checksum(seg ,src_addr,dst_addr)
+        self.servidor.rede.enviar(seg_checksum_ver, dst_addr)
+        self.servidor.close(self.id_conexao)
         pass
